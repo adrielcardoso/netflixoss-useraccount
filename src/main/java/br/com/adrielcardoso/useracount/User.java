9673fb13d98d48bed2cd6919bae8ac9be6b9fb69
ctima <ctima@MacBook-Air-de-ctima.local>
2018-07-06 14:00:36 -0300

    asdsad

--- src/main/java/br/com/adrielcardoso/useracount/User.java
+++ src/main/java/br/com/adrielcardoso/useracount/User.java
@@ -1,10 +1,42 @@
 package br.com.adrielcardoso.useracount;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "\"User\"")
-public class User {
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String userName;
+    @NotNull
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
